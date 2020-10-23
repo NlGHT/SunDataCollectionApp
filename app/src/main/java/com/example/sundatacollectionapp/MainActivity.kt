eccc133945cedfc4d2a9918109dc554f25a67d17
Anna <amaj17@student.aau.dk>
2020-10-23 14:21:22 +0200

    :)

--- app/src/main/java/com/example/sundatacollectionapp/MainActivity.kt
+++ app/src/main/java/com/example/sundatacollectionapp/MainActivity.kt
@@ -19,13 +19,11 @@ import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
 import android.widget.Button
-import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import androidx.core.content.FileProvider
-import com.google.android.material.floatingactionbutton.FloatingActionButton
 import java.io.File
 import java.io.IOException
 import java.text.SimpleDateFormat
@@ -34,7 +32,8 @@ import java.util.*
 class MainActivity : AppCompatActivity(), SensorEventListener  {
     private val REQUEST_IMAGE_CAPTURE = 1
     private lateinit var currentPhotoPath: String
-
+    private var timesun: String? = null
+    private val intentOK = Intent(this, MainActivity2::class.java)
     private lateinit var mSensorManager: SensorManager
     private var sAccelerometer: Sensor? = null
     private var vAccellerometer = FloatArray(3)
@@ -59,6 +58,8 @@ class MainActivity : AppCompatActivity(), SensorEventListener  {
     private var longitude: Double? = 0.0
     private var gpsRunning = false;
 
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -66,6 +67,8 @@ class MainActivity : AppCompatActivity(), SensorEventListener  {
 
         //findViewById<Button>(R.id.btnCamera).setOnClickListener { view ->
         //            dispatchTakePictureIntent()
+
+
 
         findViewById<Button>(R.id.btnSun).setOnClickListener { view ->
             dispatchTakePictureIntent()
@@ -256,7 +259,11 @@ class MainActivity : AppCompatActivity(), SensorEventListener  {
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
         if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
-
+            Log.v("pls work", "it does")
+            val intent = Intent(this, MainActivity2::class.java)
+            //intent.putExtra("sunTime", timesun)
+            startActivity(intent)
+            //startActivity(intentOK)
             // Save the data in here
         }
     }
@@ -264,10 +271,15 @@ class MainActivity : AppCompatActivity(), SensorEventListener  {
     @Throws(IOException::class)
     private fun createImageFile(): File {
         // Create an image file name
+
         val timeStamp: String = SimpleDateFormat("yyyyMMdd_HHmmss").format(Date())
         val storageDir: File? = getExternalFilesDir(Environment.DIRECTORY_PICTURES)
+        timesun = timeStamp
+        //val intent = Intent(this@MainActivity,MainActivity2::class.java)
+        //intent.putExtra("SunTime",timesun)
+        //startActivity(intent)
         return File.createTempFile(
-            "JPEG_${timeStamp}_", /* prefix */
+            "SUN_${timeStamp}_", /* prefix */
             ".jpg", /* suffix */
             storageDir /* directory */
         ).apply {
---
+++ app/src/main/java/com/example/sundatacollectionapp/MainActivity2.kt
@@ -0,0 +1,72 @@
+package com.example.sundatacollectionapp
+
+import android.Manifest
+import android.content.Context
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.hardware.Sensor
+import android.hardware.SensorEvent
+import android.hardware.SensorEventListener
+import android.hardware.SensorManager
+import android.location.Location
+import android.location.LocationListener
+import android.location.LocationManager
+import android.net.Uri
+import android.os.Bundle
+import android.os.Environment
+import android.provider.MediaStore
+import android.util.Log
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Button
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import androidx.core.content.FileProvider
+import java.io.File
+import java.io.IOException
+import java.text.SimpleDateFormat
+import java.util.*
+
+
+
+class MainActivity2 : AppCompatActivity() {
+    //val sunFile: String?  =intent.getStringExtra("SunTime")
+
+    private val REQUEST_IMAGE_CAPTURE = 1
+    private lateinit var currentPhotoPath: String
+    private var timesun: String? = null
+
+    //private val intentOK = Intent(this, MainActivity2::class.java)
+    private lateinit var mSensorManager: SensorManager
+    private var sAccelerometer: Sensor? = null
+    private var vAccellerometer = FloatArray(3)
+    private var sRotationVectors: Sensor? = null
+    private val rotationMatrix = FloatArray(9)
+    private var sAmbTemp: Sensor? = null
+    private var fAmbTemp: Float? = 0.0f
+    private var sAmbientLight: Sensor? = null
+    private var fAmbientLight: Float? = 0.0f
+    private var sGyroscope: Sensor? = null
+    private var vGyroscope = FloatArray(3)
+    private var sMagneticField: Sensor? = null
+    private var mMagneticField = FloatArray(3)
+    private var sGravity: Sensor? = null
+    private var mGravity = FloatArray(3)
+    private var resume = false;
+
+    private lateinit var locationManager: LocationManager
+    private lateinit var locationListener: LocationListener
+    private val locationPermissionCode = 2
+    private var latitude: Double? = 0.0
+    private var longitude: Double? = 0.0
+    private var gpsRunning = false;
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main2)
+        setSupportActionBar(findViewById(R.id.toolbar))
+    }
+}
